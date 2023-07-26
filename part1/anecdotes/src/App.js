1c91239c06627a60b0f4af004ab280b32c398c11
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-07-26 03:33:25 -0500

    am able to isolate it

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -9,14 +9,15 @@ function random(max,pre){
   
   return ran;
 }
-const Random = (props)=>{
+const Button = (props)=>{
 
   return (
     <button onClick={props.handleClick}>
-      next anecdotes
+      {props.text}
     </button>
   ) 
 }
+
 
 
 const App = () => {
@@ -34,11 +35,33 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   let ran = random(anecdotes.length, selected)
 
+  let bla =ran;
+ 
+ 
+  let arr = Array.apply(null, Array(anecdotes.length)).map(function () { return 0; });
+  const [votes, setvotes] = useState(arr);
+
+  function handleIncrementClick(index) {
+    const nextVotes = votes.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // The rest haven't changed
+        return c;
+      }
+    });
+    setvotes(nextVotes);
+  }
   return (
     <div>
       {anecdotes[selected]}
+      <br></br>
+      has {votes[1]} votes
+      
       <div>
-        <Random handleClick={()=>{setSelected(ran);}} />
+        <Button handleClick={() =>{handleIncrementClick(1);}} text="vote"/>
+        <Button handleClick={()=>{setSelected(ran);}} text= "next anecdotes" />
       </div>
     </div>
   )
