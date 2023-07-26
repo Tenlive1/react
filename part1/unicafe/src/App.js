9758e6a0fbcaac7ef9e53c26fea4db12bdd0efc0
Perry <93749717+Tenlive1@users.noreply.github.com>
2023-07-26 00:24:02 -0500

    doing 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -25,22 +25,63 @@ const Review =(props) =>{
     </div>
   )
 }
+const Total = (props) =>{
+  return(
+    <div>
+      all {props.all}
+    </div>
+  )
+}
+const Average =(props) =>{
+  return(
+    <div>
+      {props.text} {props.average}
+    </div>
+  )
+}
+const Positive = (props) =>{
+  return(
+    <div>
+      {props.text} {props.average} %
+    </div>
+  )
+}
+const Statistics = (props) =>{
+  return(
+    <>
+    <Review review ={props.good} text = "good"/>
+    <Review review ={props.neutral} text = "neutral"/>
+    <Review review ={props.bad} text = "bad"/>
+    <Total all={props.all}/>
+    <Average text ="average" average={(props.good-props.bad) / props.all}/>
+    <Positive text = "positive" average = {(props.good / props.all) * 100}/>
+    </>
+
+
+
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
   return (
     <div>
       <Header text = "give feedback"/>
-      <Button handleClick={() =>{setGood(good +1)}} text ="good"/>
-      <Button handleClick={() =>{setNeutral(neutral +1)}} text ="neutral"/>
-      <Button handleClick={() =>{setBad(bad +1)}} text ="bad"/>
+      <Button handleClick={() =>{setGood(good +1); setAll(all+1)}} text ="good"/>
+      <Button handleClick={() =>{setNeutral(neutral +1); setAll(all+1)}} text ="neutral"/>
+      <Button handleClick={() =>{setBad(bad +1); setAll(all+1)}} text ="bad"/>
       <Header text = "statistics"/>
+      
+      <Statistics good={good} neutral={neutral} bad ={bad} all ={all}/>
+      
+      
 
-      <Review review ={good} text = "good"/>
-      <Review review ={neutral} text = "neutral"/>
-      <Review review ={bad} text = "bad"/>
+      
 
     </div>
   )
